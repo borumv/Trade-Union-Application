0e4aca1c4c7ba95fa11a02f969b8ad8374bd4de1
Falltroy <fallTroy@protonmail.com>
2023-07-17 14:43:41 +0300

    Create Repositories javadocs close #3

--- back/src/main/java/backend/persist/repositories/TradeUnionRepo.java
+++ back/src/main/java/backend/persist/repositories/TradeUnionRepo.java
@@ -6,9 +6,35 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TradeUnionRepo extends PagingAndSortingRepository<TradeUnion, Integer> {
+
+    /**
+     * Retrieves an Optional of TradeUnion object by the specified union ID.
+     *
+     * @param unionId the ID of the trade union
+     * @return an Optional of TradeUnion object
+     */
     Optional<TradeUnion> findById(int unionId);
+
+    /**
+     * Saves the specified TradeUnion object.
+     *
+     * @param tradeUnion the TradeUnion object to be saved
+     * @return the saved TradeUnion object
+     */
     TradeUnion save(TradeUnion tradeUnion);
-    List<TradeUnion>findAll();
+
+    /**
+     * Retrieves a list of all TradeUnion objects.
+     *
+     * @return a list of all TradeUnion objects
+     */
+    List<TradeUnion> findAll();
+
+    /**
+     * Deletes a TradeUnion object by the specified ID.
+     *
+     * @param id the ID of the trade union to be deleted
+     */
     void deleteById(int id);
 
 }
--- back/src/main/java/backend/persist/repositories/UserRepo.java
+++ back/src/main/java/backend/persist/repositories/UserRepo.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepo extends CrudRepository<User,Long> {
+public interface UserRepo extends CrudRepository<User, Long> {
+
+     /**
+      * Retrieves an Optional of User object by the specified email.
+      *
+      * @param email the email of the user
+      * @return an Optional of User object
+      */
      Optional<User> findByEmail(String email);
 }
