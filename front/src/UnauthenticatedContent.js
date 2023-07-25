d554df2583327692422560cc8b59ace4577f9387
Falltroy <fallTroy@protonmail.com>
2023-07-25 10:37:30 +0300

    change refresh token logic in front end part

--- front/src/UnauthenticatedContent.js
+++ front/src/UnauthenticatedContent.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
-import  Authentication from './node_modules/pages/authentication/index';
+import { BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import Authentication from './node_modules/pages/authentication/index';
 
 export default function UnauthenticatedContent() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <>
       <Routes>
-        <Route path='/login'  element={<Authentication/>}/>
-        <Route path='/register'  element={<Authentication/>}/>    
+        <Route path="/login" element={<Authentication />} />
+        <Route path="/register" element={<Authentication />} />
       </Routes>
-   </>
-     
+    </>
   );
 }
